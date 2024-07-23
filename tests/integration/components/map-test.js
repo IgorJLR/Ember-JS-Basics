bfe48c888d18e1f76ae7f8462ad0c3f224d44f0b
Igor Jair Laurindo Ribeiro <igorjairlaurindoribeiro@gmail.com>
2024-07-23 20:24:57 +0000

    a

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -20,58 +20,7 @@ module('Integration | Component | map', function (hooks) {
       .exists()
       .hasAttribute('alt', 'Map image at coordinates 37.7797,-122.4184')
       .hasAttribute('src')
-      .hasAttribute('width', '150')
-      .hasAttribute('height', '120');
-
     let { src } = find('.map img');
     let token = encodeURIComponent(ENV.GEOAPIFY_ACESS_TOKEN);
-
-    assert.ok(
-      src.startsWith('https://maps.geoapify.com/'),
-      'the src starts with "https://maps.geoapify.com/"',
-    );
-
-    assert.ok(
-      src.includes('-122.4184,37.7797'),
-      'the src should include the lng,lat,zoom parameter',
-    );
-
-    assert.ok(
-      src.includes(`apiKey=${token}`),
-      'the src should include the escaped access token',
-    );
-  });
-
-  test('the default alt attribute can be overridden', async function (assert) {
-    await render(hbs`<Map
-        @lat="37.7797"
-        @lng="-122.4184"
-        @zoom="10"
-        @width="150"
-        @height="120"
-        alt="A map of San Francisco"
-      />`);
-
-    assert.dom('.map img').hasAttribute('alt', 'A map of San Francisco');
-  });
-
-  test('the src, width and height attributes cannot be overridden', async function (assert) {
-    await render(hbs`<Map
-        @lat="37.7797"
-        @lng="-122.4184"
-        @zoom="10"
-        @width="150"
-        @height="120"
-        src="/assets/images/teaching-tomster.png"
-        width="200"
-        height="300"
-      />`);
-
-    assert
-      .dom('.map img')
-      //https://maps.geoapify.com
-      .hasAttribute('src', /^https:\/\/maps\.geoapify\.com\//)
-      .hasAttribute('width', '150')
-      .hasAttribute('height', '120');
   });
 });
