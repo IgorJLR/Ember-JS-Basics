6cd6a4a74c8c2cb2f45a10db06f3e4b9ce2679e4
Igor Jair Laurindo Ribeiro <igorjairlaurindoribeiro@gmail.com>
2024-07-23 19:56:59 +0000

    correção dos testes

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -32,18 +32,46 @@ module('Integration | Component | map', function (hooks) {
     );
 
     assert.ok(
-      src.includes('-122.4184,37.7797,10'),
+      src.includes('-122.4184,37.7797'),
       'the src should include the lng,lat,zoom parameter',
     );
 
     assert.ok(
-      src.includes('150x120@2x'),
-      'the src should include the width,height and @2x parameter',
-    );
-
-    assert.ok(
-      src.includes(`access_token=${token}`),
+      src.includes(`apiKey=${token}`),
       'the src should include the escaped access token',
     );
+  });
+
+  test('the default alt attribute can be overridden', async function (assert) {
+    await render(hbs`<Map
+        @lat="37.7797"
+        @lng="-122.4184"
+        @zoom="10"
+        @width="150"
+        @height="120"
+        alt="A map of San Francisco"
+      />`);
+
+    assert.dom('.map img').hasAttribute('alt', 'A map of San Francisco');
+  });
+
+  test('the src, width and height attributes cannot be overridden', async function (assert) {
+    await render(hbs`<Map
+        @lat="37.7797"
+        @lng="-122.4184"
+        @zoom="10"
+        @width="150"
+        @height="120"
+        src="/assets/images/teaching-tomster.png"
+        width="200"
+        height="300"
+      />`);
+
+    assert
+      .dom('.map img')
+      //https://maps.geoapify.com
+      .hasAttribute('src', /^https:\/\/maps\.geoapify\.com\//)
+      .hasAttribute('width', '150')
+      .hasAttribute('height', '120');
   });
 });
